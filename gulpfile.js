ef6a6d114029a27ca70a9e80052b33cc7f6f2b06
Tomm Huth <tomm.huth@makingwaves.com>
2016-04-12 09:27:20 +0200

    drop gulp react + scripts should be under js/

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,5 @@
  * Created by tomm.huth on 11/04/16.
  */
 var gulp = require('gulp');
-var react = require('gulp-react');
 
-gulp.task('default', function () {
-    return gulp.src('app/components/app.jsx')
-        .pipe(react({harmony: false}))  // enable harmony features
-        .pipe(gulp.dest('build/js'));
-});
+gulp.task('default', function () {  });
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     entry: "./app/components/App.jsx",
     output: {
-        filename: "public/bundle.js"
+        filename: "public/js/bundle.js"
     },
     module: {
         loaders:[
