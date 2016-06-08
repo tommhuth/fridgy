1881f3f281fbd84f73ac377c05c66a958ec53a15
Tomm Huth <tomm.huth@hotmail.com>
2016-06-09 01:32:25 +0200

    dropping minify js for now

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,12 @@
-/**
- * Created by tomm.huth on 11/04/16.
- */
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefix = require("gulp-autoprefixer");
 const rename = require("gulp-rename");
-const minify = require("gulp-uglify");
 const svgstore = require('gulp-svgstore');
 const svgmin = require('gulp-svgmin');
 const path = require('path');
 
-gulp.task('default', ['sass:watch', 'sass', "js:watch", "icons:watch", "icons"]);
+gulp.task('default', ['sass:watch', 'sass', "icons:watch", "icons"]);
 
 gulp.task('sass', function () {
     return gulp.src('./resources/sass/app.scss')
@@ -21,17 +17,6 @@ gulp.task('sass', function () {
 
 gulp.task('sass:watch', function () {
     gulp.watch('./resources/sass/**/*.scss', ['sass']);
-});
-
-gulp.task('js:watch', function () {
-    gulp.watch('./public/js/bundle.js', ['minify']);
-});
-
-gulp.task('minify', function () {
-    return gulp.src('./public/js/bundle.js')
-        .pipe(minify())
-        .pipe(rename("bundle.min.js"))
-        .pipe(gulp.dest('./public/js/'));
 });
 
 gulp.task('icons:watch', function () {
