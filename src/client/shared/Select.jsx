78fb6dc2ff2b10df629009c0cb1aa16339f5a6e7
Tomm Huth <tomm.huth@hotmail.com>
2016-06-09 01:50:26 +0200

    typo

--- src/client/shared/Select.jsx
+++ src/client/shared/Select.jsx
@@ -53,7 +53,7 @@ class Select extends Component {
     render() {
         let selectClass = classNames("select", {
                 "has-focus": this.state.hasFocus,
-                "is-subtle": this.props.isSubtile
+                "is-subtle": this.props.isSubtle
             }
         );
 
