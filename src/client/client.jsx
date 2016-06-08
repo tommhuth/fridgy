8443525485c1beb2cdb8f13a2a9218e8ca9332ff
Tomm Huth <tomm.huth@hotmail.com>
2016-06-09 01:34:39 +0200

    fastclick for button style to work properly

--- src/client/client.jsx
+++ src/client/client.jsx
@@ -1,3 +1,4 @@
+import reactFastClick from "react-fastclick";
 import thunkMiddleware from 'redux-thunk'
 import React, { Component } from "react";
 import ReactDOM from 'react-dom'
@@ -5,7 +6,7 @@ import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import fridgeStore from "./app/store";
 import { Router, browserHistory } from 'react-router'
-import routes from "./routes";  
+import routes from "./routes";
 
 let store = createStore(fridgeStore, applyMiddleware(thunkMiddleware));
   
