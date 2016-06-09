c7ea3d908691c0506d836b31ad6e4114a9a8389b
Tomm Huth <tomm.huth@hotmail.com>
2016-06-09 20:20:39 +0200

    button fixes

--- src/client/shared/Button.jsx
+++ src/client/shared/Button.jsx
@@ -1,12 +1,34 @@
 import React, { Component } from "react";
     
 class Button extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            hasFocus: false
+        }
+    }
+    setFocus(){
+        this.setState({
+            hasFocus: true
+        });
+    }
+    setBlur(){
+        this.setState({
+            hasFocus: false
+        });
+    }
     render() {
+        let className = "button " + (this.state.hasFocus ? "has-focus" : "") + (this.props.className || "");
         return ( 
-                <button type="button" 
-                        onClick={this.props.onClick} 
-                        className={"button " + (this.props.className || "")}> 
-                    <span className="inner">{this.props.children}</span>
+                <button type="button"
+                        onClick={this.props.onClick}
+                        className={className}>
+                    <span className="inner"
+                          onTocuhStart={this.setFocus.bind(this)}
+                          onBlur={this.setBlur.bind(this)}>
+                        {this.props.children}
+                    </span>
                 </button> 
         )
     }
--- src/client/shared/ButtonLink.jsx
+++ src/client/shared/ButtonLink.jsx
@@ -2,12 +2,34 @@ import React, { Component } from "react";
 import { Link } from "react-router";
 
 class ButtonLink extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            hasFocus: false
+        }
+    }
+    setFocus(){
+        this.setState({
+            hasFocus: true
+        });
+    }
+    setBlur(){
+        this.setState({
+            hasFocus: false
+        });
+    }
     render() {
+        let className = "button " + (this.props.className || "");
         return (
                 <Link to={this.props.to}
                       onClick={this.props.onClick}
-                      className={"button " + (this.props.className || "")}>
-                    <span className="inner">{this.props.children}</span>
+                      className={className}>
+                    <span className="inner"
+                          onTocuhStart={this.setFocus.bind(this)}
+                          onBlur={this.setBlur.bind(this)}>
+                        {this.props.children}
+                    </span>
                 </Link> 
         )
     }
