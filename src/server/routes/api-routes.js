aede0767685bc58a3913366f1a8947f4621965eb
Tomm Huth <tomm.huth@hotmail.com>
2016-09-03 20:39:17 +0200

    renaming

---
+++ src/server/routes/api-routes.js
@@ -0,0 +1,19 @@
+"use strict";
+
+import express from "express";
+import { default as itemRouter } from  "./item-routes";
+import { default as categoriesRouter } from "./category-routes";
+import { default as unitsRouter } from "./unit-routes";
+
+let router = express.Router();
+
+router.use("/", (req, res, next) => {
+    // haha
+    setTimeout(next, Math.random() * 150);
+});
+router.use("/items", itemRouter);
+router.use("/categories", categoriesRouter);
+router.use("/units", unitsRouter);
+
+
+export default router;
