7ed5723efa5a99e60a60f431189ed592b3126cb8
Tomm Huth <tomm.huth@hotmail.com>
2016-09-03 20:44:57 +0200

    _id is now id

--- src/client/items/ItemsList.jsx
+++ src/client/items/ItemsList.jsx
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 import ListItem from "./ListItem";
 
 class ItemsList extends Component {
-    render() {
+    render() { 
 
         return (
             <div>
                 <ul className="items-list">
                     {
-                        this.props.items.map( item => <li key={item._id}> <ListItem item={item}  /> </li>)
+                        this.props.items.map( item => <li key={item.id}> <ListItem item={item}  /> </li>)
                     }
                 </ul>
             </div>
