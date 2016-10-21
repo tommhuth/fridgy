77b008d622be96a40b170035ffc9fdc0eae1afb5
Tomm Huth <tomm.huth@hotmail.com>
2016-10-21 23:38:28 +0200

    watchless task

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,34 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const autoprefix = require("gulp-autoprefixer");
-const rename = require("gulp-rename");
-const svgstore = require('gulp-svgstore');
-const svgmin = require('gulp-svgmin');
-const path = require('path');
+const gulp = require("gulp")
+const sass = require("gulp-sass")
+const autoprefix = require("gulp-autoprefixer")
+const rename = require("gulp-rename")
+const svgstore = require("gulp-svgstore")
+const svgmin = require("gulp-svgmin") 
 
-gulp.task('default', ['sass:watch', 'sass', "icons:watch", "icons"]);
+gulp.task("default", ["sass:watch", "sass", "icons:watch", "icons"])
 
-gulp.task('sass', function () {
-    return gulp.src('./resources/sass/app.scss')
-        .pipe(sass().on('error', sass.logError))
+gulp.task("build", [ "sass", "icons"])
+
+gulp.task("sass", function () {
+    return gulp.src("./resources/sass/app.scss")
+        .pipe(sass().on("error", sass.logError))
         .pipe(autoprefix())
-        .pipe(gulp.dest('./public/css'));
-});
+        .pipe(gulp.dest("./public/css"))
+})
 
-gulp.task('sass:watch', function () {
-    gulp.watch('./resources/sass/**/*.scss', ['sass']);
-});
+gulp.task("sass:watch", function () {
+    gulp.watch("./resources/sass/**/*.scss", ["sass"])
+})
 
-gulp.task('icons:watch', function () {
-    gulp.watch('./resources/icons/*', ['icons']);
-});
+gulp.task("icons:watch", function () {
+    gulp.watch("./resources/icons/*", ["icons"])
+})
 
-gulp.task('icons', function () {
+gulp.task("icons", function () {
     return gulp
-        .src('resources/icons/*.svg')
+        .src("resources/icons/*.svg")
         .pipe(svgmin())
         .pipe(svgstore())
         .pipe(rename("iconset.svg"))
-        .pipe(gulp.dest('public/gfx'));
-});
+        .pipe(gulp.dest("public/gfx"))
+})
