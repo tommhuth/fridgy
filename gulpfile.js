9a89a477c20f4c2ff83a6f996ecc8da42904336c
Tomm Huth <tomm.huth@hotmail.com>
2016-10-23 03:41:43 +0200

    fat arrows

--- gulpfile.js
+++ gulpfile.js
@@ -3,28 +3,28 @@ const sass = require("gulp-sass")
 const autoprefix = require("gulp-autoprefixer")
 const rename = require("gulp-rename")
 const svgstore = require("gulp-svgstore")
-const svgmin = require("gulp-svgmin") 
+const svgmin = require("gulp-svgmin")
 
 gulp.task("default", ["sass:watch", "sass", "icons:watch", "icons"])
 
-gulp.task("build", [ "sass", "icons"])
+gulp.task("build", ["sass", "icons"])
 
-gulp.task("sass", function () {
+gulp.task("sass", () => {
     return gulp.src("./resources/sass/app.scss")
         .pipe(sass().on("error", sass.logError))
         .pipe(autoprefix())
         .pipe(gulp.dest("./public/css"))
 })
 
-gulp.task("sass:watch", function () {
+gulp.task("sass:watch", () => {
     gulp.watch("./resources/sass/**/*.scss", ["sass"])
 })
 
-gulp.task("icons:watch", function () {
+gulp.task("icons:watch", () => {
     gulp.watch("./resources/icons/*", ["icons"])
 })
 
-gulp.task("icons", function () {
+gulp.task("icons", () => {
     return gulp
         .src("resources/icons/*.svg")
         .pipe(svgmin())
