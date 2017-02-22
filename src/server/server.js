99304a92fb27313ef79bc0b0e8dda8aea69eec4a
Tomm Huth <tomm.huth@makingwaves.com>
2017-02-22 16:11:01 +0100

    path fix

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,7 @@
 import "babel-polyfill"
 import express from "express"
 import bodyParser from "body-parser"
+import path from "path"
 import apiRoutes from "./routes/api-routes"
 import appRoutes from "./routes/app-routes"
 import mustache from "mustache-express"
@@ -14,20 +15,20 @@ import * as globalErrorHandlers from "./routes/global-error-handlers"
 
 export const app = express()
 const log = debug("fridgy-server")
-let server 
+let server
 
 export function start() {
     return connector.connect()
         .then(() => {
             return new Promise((resolve) => {
-                server = app.listen(config.PORT, () => {  
+                server = app.listen(config.PORT, () => {
                     log(`Ready @ localhost:${config.PORT}`, baseConfig)
                     resolve(app)
                 })
-            }) 
+            })
         })
         .then(() => {
-            if(config.SEED) { 
+            if (config.SEED) {
                 return seeder()
             }
         })
@@ -42,18 +43,18 @@ export function close() {
 
         resolve()
     })
-} 
+}
 
 // settings
 app.engine("mustache", mustache())
 app.use(compression())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.set("views", "../resources/views")
+app.set("views", path.join(__dirname, "..", "resources/views"))
 app.set("view engine", "mustache")
 
 // static files
-app.use(serveStatic("../public", { maxAge: "1 day" }))
+app.use(serveStatic(path.join(__dirname, "..", "public"), { maxAge: "1 day" }))
 
 //routes
 app.use("/api", apiRoutes)
@@ -66,7 +67,7 @@ app.use(globalErrorHandlers.error)
 app.use(globalErrorHandlers.notFound)
 
 // start
-if(!config.NODE_ENV.includes("test")){
+if (!config.NODE_ENV.includes("test")) {
     start()
 }
 
