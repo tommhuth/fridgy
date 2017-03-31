fb6f03ac91228deca510884ec25ee145f6e57074
Tomm Huth <tomm.huth@makingwaves.com>
2017-03-31 20:40:51 +0200

    button cleanup

--- src/client/shared/Button.js
+++ src/client/shared/Button.js
@@ -1,35 +1,14 @@
 import React, { Component } from "react"
 
 export default class Button extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            hasFocus: false
-        }
-    }
-    setFocus() {
-        this.setState({
-            hasFocus: true
-        })
-    }
-    setBlur() {
-        this.setState({
-            hasFocus: false
-        })
-    }
     render() {
-        let className = "button " + (this.state.hasFocus ? "button--focus" : "")
-    
         return (
-            <button 
+            <button
                 type="button"
+                ref={ref => this.ref = ref}
                 onClick={this.props.onClick}
-                className={className}>
-                <span 
-                    className={"button__inner " + (this.props.isInverted ? "button__inner--inverted" : "")}
-                    onTouchStart={this.setFocus.bind(this)}
-                    onBlur={this.setBlur.bind(this)}>
+                className="button">
+                <span className={"button__inner " + (this.props.isInverted ? "button__inner--inverted" : "")} >
                     {this.props.children}
                 </span>
             </button>
