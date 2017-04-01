3b4ef723ce2c08cfc375185567c5caaef8091a37
Tomm Huth <tomm.huth@makingwaves.com>
2017-04-01 22:31:43 +0200

    sorry edge

--- src/client/shared/Icon.js
+++ src/client/shared/Icon.js
@@ -1,5 +1,4 @@
-import React, { Component } from "react"
-import uuid from "node-uuid"
+import React, { Component } from "react" 
 
 export const IconType = {
     ArrowRight: "arrow-right",
@@ -39,19 +38,8 @@ const ViewBox = {
     [IconType.Fridge]: "239 364"
 }
 
-export default class Icon extends Component {
-    id = uuid.v1()
-
-    render() {
-        /*
-            Edge refuses to render svg properly on change (size is set to viewBox not 
-            CSS size), this is a super dirty fix that forces rerender of elements, 
-            suspect a mere change of attributes is not enough
-        */
-        if (/Edge\/\d./i.test(navigator.userAgent)) {
-            this.id = uuid.v1()
-        } 
-
+export default class Icon extends Component { 
+    render() { 
         return (
             <svg key={this.id} className={"icon " + (this.props.size || "")} viewBox={`0 0 ${ViewBox[this.props.type] || "0 0"}`} >
                 <use xlinkHref={ `/gfx/iconset.svg?v=${process.env.ASSETS_VERSION}#${this.props.type}`} />
