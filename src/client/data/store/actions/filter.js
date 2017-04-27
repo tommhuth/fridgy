391ef1162b17191f8bf422dbd3c5aedf3655dc95
Tomm Huth <tomm.huth@makingwaves.com>
2017-04-27 23:49:26 +0200

    moved filter back

--- src/client/data/store/actions/filter.js
+++ src/client/data/store/actions/filter.js
@@ -1,5 +1,11 @@
 import * as filterActions from "./creators/filter"
 
+export const StockFilter = {
+    All: "ALL",
+    OutOfStock: "OUT_OF_STOCK",
+    InStock: "IN_STOCK"
+}
+
 export function setCategoryFilter(category) {
     return (dispatch) => {
         dispatch(filterActions.setCategoryFilter(category))
@@ -10,4 +16,23 @@ export function setStockFilter(stock) {
     return (dispatch) => {
         dispatch(filterActions.setStockFilter(stock))
     }
+}
+
+export function filterItems(items, filter) {
+    let filtered = [...items]
+
+    switch (filter.stock) {
+        case StockFilter.InStock:
+            filtered = filtered.filter(e => e.amount > 0)
+            break
+        case StockFilter.OutOfStock:
+            filtered = filtered.filter(e => !e.amount )
+            break
+    }
+
+    if (filter.category) {
+        filtered = filtered.filter(e => e.category.toLowerCase() === filter.category.toLowerCase())
+    }
+
+    return filtered
 }
