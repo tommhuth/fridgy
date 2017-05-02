7a9ade1657e5296c8280cca749f97323ed628d69
Tomm Huth <tomm.huth@makingwaves.com>
2017-05-02 16:54:03 +0200

    if already loaded, skip

--- src/client/font-loader.js
+++ src/client/font-loader.js
@@ -1,5 +1,7 @@
 import FontFaceObserver from "fontfaceobserver"
+import LocalStorage from "./data/LocalStorage"
 
+const key = "fridgy-fonts-loaded"
 const helvetica700 = new FontFaceObserver("Neue Helvetica W01", { weight: 700 }).load()
 const helvetica400 = new FontFaceObserver("Neue Helvetica W01", { weight: 400 }).load()
 const didotItalic = new FontFaceObserver("Linotype Didot W01", { style: "italic" }).load()
@@ -8,10 +10,16 @@ async function load() {
     try {
         await Promise.all([helvetica700, helvetica400, didotItalic])
 
-        document.documentElement.className += "fonts-loaded"
-    } catch (e) {
+        document.documentElement.className = "fonts-loaded"
+        LocalStorage.set(key, true)
+    } catch(e) {
         // oh no! only fallback fonts
+        LocalStorage.remove(key)
     }
 }
 
-load()
+if (!LocalStorage.get(key)) {
+    load()
+} else {
+    document.documentElement.className = "fonts-loaded"
+}
